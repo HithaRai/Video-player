8961ab7d07c88669698d470ff8aed0be0d023186
Hitha Rai <hitha.rai@ymedialabs.com>
2019-11-29 10:51:48 +0530

    basic functionality update

--- src/containers/VideoPlayer/videoPlayer.stories.js
+++ src/containers/VideoPlayer/videoPlayer.stories.js
@@ -292,3 +292,41 @@ export const withVideo_poster_without_autoplay_and_controls_withoutAd = () => (
 		}}
 	/>
 );
+
+export const withVideo_poster_without_autoplay_and_controls_withAd = () => (
+	<VideoPlayer
+		src='https://bitdash-a.akamaihd.net/content/sintel/hls/playlist.m3u8'
+		addSrc={{
+			url: [
+				'http://clips.vorwaerts-gmbh.de/VfE_html5.mp4',
+				'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4',
+				// 'https://bitdash-a.akamaihd.net/content/sintel/hls/playlist.m3u8',
+				// 'https://bitdash-a.akamaihd.net/content/sintel/hls/playlist.m3u8',
+				// 'http://clips.vorwaerts-gmbh.de/VfE_html5.mp4',
+				// 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/Sintel.mp4',
+				// 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4',
+				'http://clips.vorwaerts-gmbh.de/VfE_html5.mp4'
+			],
+			showSkipAdEnable: 5,
+			skippable: true
+		}}
+		description='This is a demo video of m3u8 format '
+		poster='https://www.themarysue.com/wp-content/uploads/2016/02/sale_3342_special_banner_background.jpg'
+		title='Playing First Video'
+		isAutoPlay={false}
+		isControls={true}
+		configurations={{
+			width: '300px',
+			minHeight: '100px',
+			maxHeight: '700px',
+			// left: '100px',
+			// right: '20px',
+			// top: '100px',
+			// bottom: '20px',
+			iconColor: 'white',
+			controlBarBgColor: 'black',
+			seekBarColor: 'red',
+			fontSize: '20px'
+		}}
+	/>
+);
